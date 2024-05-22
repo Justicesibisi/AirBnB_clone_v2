cc0c48abeb99cd625e661d333a20a470d1c4a8d5
Justicesibisi <djmangsa@gmail.com>
2024-05-22 15:55:35 -0700

    Flasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,10 @@
-
-
-from flask import Flask, escape
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """
-    display "Hello HBNB!"
-    """
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    display “HBNB”
-    """
-    return "HBNB"
-
+# AirBnB_clone_v2/web_flask/2-c_route.py
+from web_flask import app
+from flask import Flask
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """
-    display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
-    text = text.replace("_", " ")
-    return "C %s" % escape(text)
+def c_is_fun(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
